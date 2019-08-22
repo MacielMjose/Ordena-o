7e8f4aa3834ae577b49d16147924bc48dc2b60e6
José Luiz Marques Maciel <41921890+MacielMjose@users.noreply.github.com>
2019-08-22 19:42:57 -0300

    criação 2 for, alteração da logica de comparação

--- OrdenaLista.cpp
+++ OrdenaLista.cpp
@@ -17,6 +17,7 @@ void ordena_lista (NODO *lista); // ordena registros por código - ordem crescen
 void ordena_lista(NODO *lista)
 {
 	int index = 0; //controla as posições do vetor de tipo lista
+	int y;
 	NODO swap; //variavel do tipo nodo para auxiliar na troca
 	
 	if(lista->f == 0) //valida se a lista está vazia, se estiver, ordenação não executa.
@@ -25,24 +26,26 @@ void ordena_lista(NODO *lista)
 			
 	}
 	else
-	{    	//*trocar o lista->f por Index
-		//*trocar a condição por index < LISTA_TAM
-		for(lista->f = 0 ; lista->f <= N_MAX ; lista->f++) //para lista começando em '0' enquanto não for de tamanho N_MAX incrementa o lista->f
+	{    //*trocar o lista->f por Index - ok
+		//*trocar a condição por index < LISTA_TAM - ok
+		for(index = 0 ; index < lista->f ; index++) //para lista começando em '0' enquanto não for de tamanho N_MAX incrementa o lista->f
 		{ //*implementar segundo for para comparar cada posição, com toda a Lista
-			if(lista[index].info->codigo  > lista[index+1].info->codigo ) //verifica desde a primeira posição, comparando se o código da próxima é menor que o da posição atual
+			for(y = 0 ; y < lista->f ; y++)
 			{
-				
-				swap.info->codigo = lista[index].info->codigo;  // swap recebe o codigo da posição Lista[index] 
-				lista[index].info->codigo = lista[index+1].info->codigo; // Lista[index] recebe o codigo de Lista[index+1] (próxima)
-				lista[index+1].info->codigo = swap.info->codigo; //Lista[index+1] recebe o código armazenado em Swap;
-					
-				index ++; //incrementa index para dar continuidade no laço FOR //*colocar o Index fora do IF
+				if(lista[index].info->codigo  > lista[y+1].info->codigo ) //verifica desde a primeira posição, comparando se o código da próxima é menor que o da posição atual
+					{
+						
+						swap.info->codigo = lista[index].info->codigo;  // swap recebe o codigo da posição Lista[index] 
+						lista[index].info->codigo = lista[y+1].info->codigo; // Lista[index] recebe o codigo de Lista[index+1] (próxima)
+						lista[y+1].info->codigo = swap.info->codigo; //Lista[index+1] recebe o código armazenado em Swap;
+							
+						//index ++; //incrementa index para dar continuidade no laço FOR //*colocar o Index fora do IF
+					}
+				y ++;
 			}
-			
-			
+			index ++;	
 		}
-		
-		
+				
 	}
 	
 	
@@ -53,5 +56,4 @@ int main()
 	
 	return 0;
 }
-
 
