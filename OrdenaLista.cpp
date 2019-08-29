00f4783992c784d9eabf37eaeaa341b641deec9b
José Luiz Marques Maciel <41921890+MacielMjose@users.noreply.github.com>
2019-08-28 21:44:21 -0300

    Subida do Arquivo contendo o metodo Consulta Nome

--- OrdenaLista.cpp
+++ OrdenaLista.cpp
@@ -13,33 +13,79 @@ typedef struct {
 }NODO; // estrutura do tipo NODO
 
 void ordena_lista (NODO *lista); // ordena registros por cÃ³digo - ordem crescente
+void inclui_inicio ( NODO *lista ); // inclui um novo registro no início da lista (posição 0)
+void exclui_nodo ( NODO *lista ); // exclui um registro por código
+void altera_nodo ( NODO *lista ); // altera um registro a partir de um código de referência
+void ordena_lista ( NODO *lista ); // ordena registros por código - ordem crescente
+void consulta_nome ( NODO lista ); // consulta por nome de referência
+
+int main()
+{
+	
+	return 0;
+}
+
+void consulta_nome ( NODO lista ) // consulta por nome de referência
+{
+	char procuraNome[30]; // o nome nao terá mas que 30 char de acordo com definição da Struct
+	int index;
+	
+	if(lista.f == 0) //valida se a lista está vazia
+	{
+		printf("Não existem registros a serem consultados!"); //informa usuario
+	}
+	
+	printf("informe o nome a ser consultado:"); //pega nome a ser consultado
+	scanf("%c",&procuraNome);//le o nome a ser consultado
+	
+	for (index = 0 ;  index < lista.f ; index ++) //percorre todas as posições do vetor enquanto nao atingir o limite de tamanho ocupado
+	{
+	
+		if(lista.info[index].nome == procuraNome) //verifica desde a primeira posição, comparando se o código da próxima é menor que o da posição atual
+			{
+			
+				printf("Nome: %c",procuraNome);	//exibe o nome consultado
+			
+			}
+		
+		if(index == lista.f) //valida se o index percorreu todas as posições do vetor
+			{
+			
+				printf("O nome não foi encontrado");//informa ao user que nome nao encontrado
+			
+			}
+		
+	}
+		
+}
+
 
 void ordena_lista(NODO *lista)
 {
-	int index = 0; //controla as posiÃ§Ãµes do vetor de tipo lista
+	int index = 0; //controla as posições do vetor de tipo lista
 	int y;
 	NODO swap; //variavel do tipo nodo para auxiliar na troca
 	
-	if(lista->f == 0) //valida se a lista estÃ¡ vazia, se estiver, ordenaÃ§Ã£o nÃ£o executa.
+	if(lista->f == 0) //valida se a lista está vazia, se estiver, ordenação não executa.
 	{
-		printf("NÃ£o existem registros para ordenaÃ§Ã£o...");
+		printf("Não existem registros para ordenação...");
 			
 	}
 	else
 	{    //*trocar o lista->f por Index - ok
-		//*trocar a condiÃ§Ã£o por index < LISTA_TAM - ok
-		for(index = 0 ; index < lista->f ; index++) //para lista comeÃ§ando em '0' enquanto nÃ£o for de tamanho N_MAX incrementa o lista->f
-		{ //*implementar segundo for para comparar cada posiÃ§Ã£o, com toda a Lista
+		//*trocar a condição por index < LISTA_TAM - ok
+		for(index = 0 ; index < lista->f ; index++) //para lista começando em '0' enquanto não for de tamanho N_MAX incrementa o lista->f
+		{ //*implementar segundo for para comparar cada posição, com toda a Lista
 			for(y = 0 ; y < lista->f ; y++)
 			{
-				if(lista->info[index].codigo  > lista->info[y+1].codigo ) //verifica desde a primeira posiÃ§Ã£o, comparando se o cÃ³digo da prÃ³xima Ã© menor que o da posiÃ§Ã£o atual
+				if(lista[index].info->codigo  > lista[y+1].info->codigo ) //verifica desde a primeira posição, comparando se o código da próxima é menor que o da posição atual
 					{
 						
-						swap.info->codigo = lista->info[index].codigo;  // swap recebe o codigo da posiÃ§Ã£o Lista[index] 
-						lista->info[index].codigo = lista->info[y+1].codigo; // Lista[index] recebe o codigo de Lista[index+1] (prÃ³xima)
-						lista->info[y+1].codigo = swap.info->codigo; //Lista[index+1] recebe o cÃ³digo armazenado em Swap;
+						swap.info->codigo = lista[index].info->codigo;  // swap recebe o codigo da posição Lista[index] 
+						lista[index].info->codigo = lista[y+1].info->codigo; // Lista[index] recebe o codigo de Lista[index+1] (próxima)
+						lista[y+1].info->codigo = swap.info->codigo; //Lista[index+1] recebe o código armazenado em Swap;
 							
-						//index ++; //incrementa index para dar continuidade no laÃ§o FOR //*colocar o Index fora do IF
+						//index ++; //incrementa index para dar continuidade no laço FOR //*colocar o Index fora do IF
 					}
 				y ++;
 			}
@@ -47,17 +93,5 @@ void ordena_lista(NODO *lista)
 		}
 				
 	}
-	
-	
+		
 }
-
-
-
-int main()
-{
-	
-	
-	
-	return 0;
-}
-
